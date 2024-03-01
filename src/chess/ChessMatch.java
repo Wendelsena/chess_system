b98142698b7fe40e90b181c0cd49abc14d6730e4
Wendelsena <wendel4019@gmail.com>
2024-03-01 16:10:51 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -26,10 +25,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	// operação para colocar peça passando a posição nas coordenadas do xadrez
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPositon()); // 
+	}
 	// inicia a partida
 	private void InitialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.BLACK));
 	}
 }
